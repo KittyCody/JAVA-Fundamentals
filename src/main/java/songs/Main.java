be8f168f3d9a162f5b0e4245e6470f04c3112d29
KittyCody <monikalna@gmail.com>
2024-09-17 16:35:59 +0200

    objects and classes

---
+++ src/main/java/songs/Main.java
@@ -0,0 +1,40 @@
+package songs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countSongs = Integer.parseInt(scanner.nextLine());
+        List<Song> songsList = new ArrayList<Song>();
+
+        for(int count = 1; count <= countSongs; count++) {
+             String data = scanner.nextLine();
+           String[] songData = data.split("_");
+            String typeList = songData[0];
+                    String title = songData[1];
+                    String time = songData[2];
+
+
+                    Song song = new Song(typeList, title, time);
+                    songsList.add(song);
+        }
+
+        String command = scanner.nextLine();
+
+        if(command.equals("all")) {
+            for(Song song : songsList) {
+                System.out.println(song.getTitle());
+            }
+        } else{
+            for(Song song : songsList) {
+                if(song.getTypeList().equals(command)){
+                    System.out.println(song.getTitle());
+                }
+            }
+        }
+        scanner.close();
+    }
+}
