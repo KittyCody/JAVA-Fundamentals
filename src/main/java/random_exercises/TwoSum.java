684104f5009e0d4e56ac79c776296f5af1c8a663
KittyCody <monikalna@gmail.com>
2025-02-19 18:45:19 +0100

    last algo

--- src/main/java/random_exercises/TwoSum.java
+++ src/main/java/random_exercises/TwoSum.java
@@ -4,6 +4,31 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class TwoSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] nums = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int target = scanner.nextInt();
+
+        int[] result = FindTwoSum.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices found: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No valid pair found.");
+        }
+
+        scanner.close();
+    }
+}
+
+class FindTwoSum {
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -14,31 +39,5 @@ public class TwoSum {
             map.put(nums[i], i);
         }
         return new int[]{};
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the size of the array:");
-        int size = scanner.nextInt();
-        int[] nums = new int[size];
-
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < size; i++) {
-            nums[i] = scanner.nextInt();
-        }
-
-        System.out.println("Enter the target:");
-        int target = scanner.nextInt();
-
-        int[] result = twoSum(nums, target);
-
-        if (result.length == 2) {
-            System.out.println("Indices found: [" + result[0] + ", " + result[1] + "]");
-        } else {
-            System.out.println("No valid pair found.");
-        }
-
-        scanner.close();
     }
 }
